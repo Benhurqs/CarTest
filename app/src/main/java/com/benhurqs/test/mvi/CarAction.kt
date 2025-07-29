04ac1bffaa2de034e766a1cc6c6cd160dd5d2b00
Benhur Souza <benhur.quintino@gmail.com>
2025-07-29 18:43:20 -0300

    Criação de modal para salvar dados do comprador

--- app/src/main/java/com/benhurqs/test/mvi/CarAction.kt
+++ app/src/main/java/com/benhurqs/test/mvi/CarAction.kt
@@ -9,5 +9,6 @@ interface CarAction {
     data class OnClickCar(val car: Car): CarAction
     data object OnClickSaveCar: CarAction
     data object OnCloseModal: CarAction
+    data object OnCloseDialog: CarAction
     data class OnSaveUser(val user: UserData): CarAction
 }
--- app/src/main/java/com/benhurqs/test/ui/components/UserFormDialog.kt
+++ app/src/main/java/com/benhurqs/test/ui/components/UserFormDialog.kt
@@ -1,11 +1,14 @@
 package com.benhurqs.test.ui.components
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
@@ -23,8 +26,12 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.benhurqs.test.viewmodel.CarViewModel
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.Role.Companion.Button
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.benhurqs.test.data.UserData
+import com.benhurqs.test.mvi.CarAction
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -32,32 +39,103 @@ fun UserFormDialog(
     viewModel: CarViewModel = hiltViewModel()
 ) {
     val uiState by viewModel.uiState.collectAsStateWithLifecycle()
-    DialogContent()
+    if (uiState.showDialog) {
+        DialogContent(
+            onDismiss = { viewModel.dispatch(CarAction.OnCloseDialog) },
+            onSave = { name, email ->
+                viewModel.dispatch(CarAction.OnSaveUser(UserData(name, email)))
+                viewModel.dispatch(CarAction.OnCloseDialog)
+            }
+        )
+    }
 }
 
 @Composable
-fun DialogContent(){
-    Dialog(onDismissRequest = {  }) {
+fun DialogContent(
+    onDismiss: () -> Unit,
+    onSave: (String, String) -> Unit
+) {
+    var name by remember { mutableStateOf("") }
+    var email by remember { mutableStateOf("") }
+
+    Dialog(onDismissRequest = { onDismiss() }) {
         Card(
             modifier = Modifier
                 .fillMaxWidth()
-                .height(200.dp)
                 .padding(16.dp),
             shape = RoundedCornerShape(16.dp),
         ) {
-            Text(
-                text = "This is a minimal dialog",
+            Column(
                 modifier = Modifier
-                    .fillMaxSize()
-                    .wrapContentSize(Alignment.Center),
-                textAlign = TextAlign.Center,
-            )
+                    .padding(16.dp)
+                    .fillMaxWidth()
+            ) {
+
+                Text(
+                    text = "Dados Para Compra",
+                    fontSize = 20.sp,
+                    modifier = Modifier.padding(bottom = 16.dp)
+                )
+
+
+                TextFieldWithTitle(
+                    title = "Nome",
+                    value = name,
+                    onValueChange = { name = it }
+                )
+
+                Spacer(modifier = Modifier.height(24.dp))
+
+
+                TextFieldWithTitle(
+                    title = "Email",
+                    value = email,
+                    onValueChange = { email = it }
+                )
+
+                Spacer(modifier = Modifier.height(10.dp))
+
+
+                Button(
+                    modifier = Modifier.fillMaxWidth(),
+                    onClick = {
+                        onSave(name, email)
+                    }
+                ) {
+                    Text(text = "Salvar", fontSize = 16.sp)
+                }
+            }
         }
+    }
+}
+
+@Composable
+fun TextFieldWithTitle(
+    title: String,
+    value: String,
+    onValueChange: (String) -> Unit
+) {
+    Column(modifier = Modifier.fillMaxWidth()) {
+        Text(
+            text = title,
+            fontSize = 16.sp,
+            modifier = Modifier.padding(bottom = 8.dp)
+        )
+        androidx.compose.material3.TextField(
+            value = value,
+            onValueChange = onValueChange,
+            modifier = Modifier.fillMaxWidth()
+        )
     }
 }
 
 @Preview(apiLevel = 34)
 @Composable
-fun PreviewModal() {
-    DialogContent()
+fun PreviewDialogContent() {
+    DialogContent(
+        onDismiss = { println("Dialog fechado") },
+        onSave = { name, email ->
+            println("Salvo: Nome = $name, Email = $email")
+        }
+    )
 }
